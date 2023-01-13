52957671080170d002eeb5239f032dc823e48a9f
Unchartedfan5409 <122555334+Unchartedfan5409@users.noreply.github.com>
2023-01-13 12:25:24 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,69 @@
 input.onButtonPressed(Button.A, function () {
     basic.showLeds(`
+        # . . . #
+        # . . . #
+        # # # # #
+        # . . . #
+        # . . . #
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
         . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
+        . . # . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        # # # # #
         `)
 })
 basic.showLeds(`
-    # # # # #
+    # # . # #
+    # . # . #
+    # . # . #
+    # . . . #
+    # . . . #
+    `)
+basic.showLeds(`
+    . . # . .
     . # . # .
+    # . . . #
     # # # # #
+    # . . . #
+    `)
+basic.showLeds(`
+    # . . . #
     . # . # .
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
+basic.showLeds(`
+    # . . . #
+    # # . . #
+    # . # . #
+    # . . # #
+    # . . . #
+    `)
+basic.showLeds(`
     # # # # #
+    # . . . #
+    # . # . #
+    # . . . #
+    # # # # #
+    `)
+basic.showLeds(`
+    # # # # .
+    # . . . #
+    # # # # .
+    # . . . #
+    # . . . #
     `)
