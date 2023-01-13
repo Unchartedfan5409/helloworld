e6cf9033d607fdf50f294bb8bb0dfd4a967a7451
Unchartedfan5409 <122555334+Unchartedfan5409@users.noreply.github.com>
2023-01-13 12:07:13 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+basic.showLeds(`
+    # # # # #
+    . # . # .
+    # # # # #
+    . # . # .
+    # # # # #
+    `)
